cc5e5dc1b0af7991698fe7ed0fc39a37590071d1
Dane Kennedy <kennedy.dane@gmail.com>
2025-05-01 21:40:04 +0200

    Add oidc, login and logout views

--- ilifu_helpdesk/ilifu_helpdesk/urls.py
+++ ilifu_helpdesk/ilifu_helpdesk/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
+from .views import login, logout
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('oidc/', include('mozilla_django_oidc.urls')),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('', include('helpdesk.urls', namespace='helpdesk')),
 ]
