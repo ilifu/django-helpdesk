f32ef1854e696fe952167faecd551309a6352c26
Dane Kennedy <kennedy.dane@gmail.com>
2025-05-02 17:13:48 +0200

    Add django debug and media urls

--- ilifu_helpdesk/ilifu_helpdesk/urls.py
+++ ilifu_helpdesk/ilifu_helpdesk/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 from .views import login, logout
 
@@ -27,3 +32,7 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     path('', include('helpdesk.urls', namespace='helpdesk')),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += debug_toolbar_urls()
