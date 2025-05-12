742b020708f87cabd396881c4284e862c44dd9b2
Dane Kennedy <kennedy.dane@gmail.com>
2025-05-12 13:10:39 +0200

    Monkey patch create_object_from_email_message so that resolved tickets are also reopened by emails

--- ilifu_helpdesk/ilifu/utils.py
+++ ilifu_helpdesk/ilifu/utils.py
@@ -1,9 +1,18 @@
-
+from email.utils import getaddresses
 import logging
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
+from django.utils import timezone
 from django.utils.html import escape, linebreaks
-from helpdesk.email import HTML_EMAIL_ATTACHMENT_FILENAME
-from helpdesk.models import FollowUp
+from django.utils.translation import gettext as _
+from helpdesk import settings as helpdesk_settings
+from helpdesk.email import create_ticket_cc, HTML_EMAIL_ATTACHMENT_FILENAME, is_autoreply, send_info_email
+from helpdesk.lib import process_attachments, safe_template_context
+from helpdesk.models import FollowUp, Ticket
+from helpdesk.signals import new_ticket_done, update_ticket_done
+
 
 logger = logging.getLogger(__name__)
 
@@ -97,3 +106,130 @@ def custom_followup_display(followup_instance: FollowUp):
         else:
             return ""
 
+def custom_create_object_from_email_message(message, ticket_id, payload, files, logger):
+    ticket, previous_followup, new = None, None, False
+    now = timezone.now()
+
+    queue = payload["queue"]
+    sender_email = payload["sender_email"]
+
+    to_list = getaddresses(message.get_all("To", []))
+    cc_list = getaddresses(message.get_all("Cc", []))
+
+    message_id = message.get("Message-Id")
+    in_reply_to = message.get("In-Reply-To")
+
+    if message_id:
+        message_id = message_id.strip()
+
+    if in_reply_to:
+        in_reply_to = in_reply_to.strip()
+
+    if in_reply_to is not None:
+        try:
+            queryset = FollowUp.objects.filter(message_id=in_reply_to).order_by("-date")
+            if queryset.count() > 0:
+                previous_followup = queryset.first()
+                ticket = previous_followup.ticket
+        except FollowUp.DoesNotExist:
+            pass  # play along. The header may be wrong
+
+    if previous_followup is None and ticket_id is not None:
+        try:
+            ticket = Ticket.objects.get(id=ticket_id)
+        except Ticket.DoesNotExist:
+            ticket = None
+        else:
+            new = False
+            # Check if the ticket has been merged to another ticket
+            if ticket.merged_to:
+                logger.info("Ticket has been merged to %s" % ticket.merged_to.ticket)
+                # Use the ticket in which it was merged to for next operations
+                ticket = ticket.merged_to
+    # New issue, create a new <Ticket> instance
+    if ticket is None:
+        if not getattr(settings, "QUEUE_EMAIL_BOX_UPDATE_ONLY", False):
+            ticket = Ticket.objects.create(
+                title=payload["subject"],
+                queue=queue,
+                submitter_email=sender_email,
+                created=now,
+                description=payload["body"],
+                priority=payload["priority"],
+            )
+            ticket.save()
+            logger.debug("Created new ticket %s-%s" % (ticket.queue.slug, ticket.id))
+            new = True
+        else:
+            # Possibly an email with no body but has an attachment
+            logger.debug(
+                "The QUEUE_EMAIL_BOX_UPDATE_ONLY setting is True so new ticket not created."
+            )
+            return None
+    # Old issue being re-opened
+    elif ticket.status in [Ticket.CLOSED_STATUS, Ticket.RESOLVED_STATUS]:
+        ticket.status = Ticket.REOPENED_STATUS
+        ticket.save()
+
+    f = FollowUp(
+        ticket=ticket,
+        title=_(
+            "E-Mail Received from %(sender_email)s" % {"sender_email": sender_email}
+        ),
+        date=now,
+        public=True,
+        comment=payload.get("full_body", payload["body"]) or "",
+        message_id=message_id,
+    )
+
+    if ticket.status == Ticket.REOPENED_STATUS:
+        f.new_status = Ticket.REOPENED_STATUS
+        f.title = _(
+            "Ticket Re-Opened by E-Mail Received from %(sender_email)s"
+            % {"sender_email": sender_email}
+        )
+
+    f.save()
+    logger.debug("Created new FollowUp for Ticket")
+
+    logger.info(
+        "[%s-%s] %s"
+        % (
+            ticket.queue.slug,
+            ticket.id,
+            ticket.title,
+        )
+    )
+
+    if helpdesk_settings.HELPDESK_ENABLE_ATTACHMENTS:
+        try:
+            attached = process_attachments(f, files)
+        except ValidationError as e:
+            logger.error(str(e))
+        else:
+            for att_file in attached:
+                logger.info(
+                    "Attachment '%s' (with size %s) successfully added to ticket from email.",
+                    att_file[0],
+                    att_file[1].size,
+                )
+
+    context = safe_template_context(ticket)
+
+    new_ticket_ccs = []
+    new_ticket_ccs.append(create_ticket_cc(ticket, to_list + cc_list))
+
+    autoreply = is_autoreply(message)
+    if autoreply:
+        logger.info(
+            "Message seems to be auto-reply, not sending any emails back to the sender"
+        )
+    else:
+        send_info_email(message_id, f, ticket, context, queue, new)
+    if new:
+        # emit signal when a new ticket is created
+        new_ticket_done.send(sender="create_object_from_email_message", ticket=ticket)
+    else:
+        # emit signal with followup when the ticket is updated
+        update_ticket_done.send(sender="create_object_from_email_message", followup=f)
+    return ticket
